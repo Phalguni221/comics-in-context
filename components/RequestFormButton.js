f9b636e40ae8cb98a450d98ca8ab59ef5b0c3c0e
Sriya Bhamidipati <88012398+Phalguni221@users.noreply.github.com>
2022-02-02 09:30:37 -0500

    comics-in-context

---
+++ components/RequestFormButton.js
@@ -0,0 +1,16 @@
+
+import React, { useContext } from "react";
+import StylesContext from "components/StylesContext"
+
+export default function CollectorButton() {
+  const buttonStyling = React.useContext(StylesContext)
+
+  return (
+    <div>
+      <button style={buttonStyling}>
+        Click Here!
+      </button>
+    </div>
+  );
+}  
+
